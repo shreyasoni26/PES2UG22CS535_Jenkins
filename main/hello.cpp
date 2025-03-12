f803b207a94d8b7a03cb60f4fe570433d59c2c18
shreyasoni26 <shreya.soni26@gmail.com>
2025-03-12 21:22:25 +0530

    First Commit

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "I am Shreya Soni PES2UG22CS535 \n";
+  return 0;
+}
